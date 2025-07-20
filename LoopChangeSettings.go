8e7e4d809710381ed74a319ace5d5f4ef6175850
pSlessing <psless23@student.aau.dk>
2025-07-20 13:21:05 +0200

    More groundwork, probably breaks github actions, oh well

--- LoopChangeSettings.go
+++ LoopChangeSettings.go
@@ -1,5 +1,30 @@
 package main
 
+import (
+	"fmt"
+	"github.com/nsf/termbox-go"
+	"reflect"
+)
+
 func ChangeSettingsLoop() {
+	noOfSettings := reflect.TypeOf(Settings{}).NumField()
+	var currentPos int
+	var currentTempSettings Settings = GetCurrentSettings()
+	ChangeMarkedSetting := func() {
+
+	}
+
+	for {
+		event := termbox.PollEvent()
+		if event.Type == termbox.EventKey {
+			switch event.Key {
+			case termbox.KeyArrowDown:
+			case termbox.KeyArrowUp:
+			case termbox.KeyArrowRight:
+			case termbox.KeyArrowLeft:
+			case termbox.KeyEnter:
+			}
+		}
+	}
 
 }
