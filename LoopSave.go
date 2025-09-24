05071b493f0d8d86d956cc19372831efcbc3eb7d
pSlessing <psless23@student.aau.dk>
2025-09-24 11:34:47 +0200

    Saving works now

--- LoopSave.go
+++ LoopSave.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/gdamore/tcell/v2"
-	"github.com/nsf/termbox-go"
 )
 
 func SaveAsLoop() string {
@@ -18,30 +17,33 @@ func SaveAsLoop() string {
 		PrintMessageStyle((COLS/2)-LINECOUNTWIDTH, (ROWS/2)+1, STYLES.MSGSTYLE, string(saveBuffer))
 		TERMINAL.Show()
 
-		event := termbox.PollEvent()
+		event := TERMINAL.PollEvent()
 
-		if event.Key == termbox.KeyEnter {
-			filename := string(saveBuffer)
-			if filename != "" {
-				err := WriteBufferToFile(TEXTBUFFER, filename)
-				if err != nil {
-					PrintMessage(0, ROWS, tcell.ColorRed, tcell.ColorDefault,
-						fmt.Sprintf("Error saving file: %s", err.Error()))
-					TERMINAL.Show()
-					termbox.PollEvent()
-				} else {
-					return filename
+		switch ev := event.(type) {
+		case *tcell.EventKey:
+			if ev.Key() == tcell.KeyEnter {
+				filename := string(saveBuffer)
+				if filename != "" {
+					err := WriteBufferToFile(TEXTBUFFER, filename)
+					if err != nil {
+						PrintMessage(0, ROWS, tcell.ColorRed, tcell.ColorDefault,
+							fmt.Sprintf("Error saving file: %s", err.Error()))
+						TERMINAL.Show()
+						TERMINAL.PollEvent()
+					} else {
+						return filename
+					}
 				}
+				return SOURCEFILE
+			} else if ev.Key() == tcell.KeyBackspace || ev.Key() == tcell.KeyBackspace2 {
+				if len(saveBuffer) > 0 {
+					saveBuffer = saveBuffer[:len(saveBuffer)-1]
+				}
+			} else if ev.Key() == tcell.KeyEscape {
+				return SOURCEFILE
+			} else if ev.Rune() != 0 {
+				saveBuffer = append(saveBuffer, ev.Rune())
 			}
-			break
-		} else if event.Key == termbox.KeyBackspace || event.Key == termbox.KeyBackspace2 {
-			if len(saveBuffer) > 0 {
-				saveBuffer = saveBuffer[:len(saveBuffer)-1]
-			}
-		} else if event.Key == termbox.KeyEsc {
-			break
-		} else {
-			saveBuffer = append(saveBuffer, event.Ch)
 		}
 	}
 	return SOURCEFILE
